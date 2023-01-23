1b1782c3fb124e951630124cb2cf3fadf5c35fd9
krishnapatel9104 <121010348+krishnapatel9104@users.noreply.github.com>
2023-01-23 15:41:22 +0530

    Delete que4.js

--- que4.js
+++
@@ -1,5 +0,0 @@
-let alphbetStr = "ABCKWQDEPFGMHODIJKALYMNCQOPQRBSTLBUVLWXYZ";
-let str = new Set(alphbetStr);
-let arrStr = Array.from(str);
-console.log("Final output : ", arrStr.sort());
-console.log("Final output other way we can do : ", [...str].sort());
